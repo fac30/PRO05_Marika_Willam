ac48e0fc276fcabe0599104265694b3f5106743f
MarikaBBB <mbertelli012@gmail.com>
2024-11-12 15:04:53 +0000

    init index.php file

---
+++ wordpress/wp-content/themes/marika-theme/templates/index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GreenHarvest Store</title>
+</head>
+
+<body>
+
+    <!-- Header -->
+    <header>
+        <nav>
+            <ul>
+                <li><a href="#about-us">About us</a></li>
+                <li><a href="#our-products">Our Products</a></li>
+                <li><a href="#contact">Contact Us</a></li>
+            </ul>
+        </nav>
+    </header>
+
+
+    <section id="hero">
+        <h1>Welcome to GreenHarvest</h1>
+        <!-- image place hold -->
+    </section>
+
+    <!-- Category Section -->
+    <section id="categories">
+        <h2>Categories</h2>
+        <ul>
+            <li>Pantry</li>
+            <li>Frozen</li>
+            <li>Meat & Seafood</li>
+            <li>Snacks, Bars & Treats</li>
+            <li>Beverages</li>
+            <li>Babies & Kids</li>
+            <li>Health</li>
+            <li>Personal Care</li>
+            <li>Home & Pet</li>
+        </ul>
+    </section>
+
+
+    <section id="sales">
+        <h2>On Sale This Week</h2>
+        <ul>
+            <li></li>
+        </ul>
+    </section>
+
+
+    <section id="testimonials">
+        <h2>testimonials</2>
+            <h3>See why more than 1 million members trust us</h3>
+            <p>grewdq</p>
+    </section>
+
+    <footer>
+        <p>Follow Us</p>
+        <ul>
+            <li><a href="#"></a></li>
+            <li><a href="#"></a></li>
+            <li><a href="#"></a></li>
+            <li><a href="#"></a></li>
+            <li><a href="#"></a></li>
+        </ul>
+
+
+        </ul>
+        <h4>We’re on a mission to make healthy living easy and affordable for everyone.</h4>
+        <ul>
+            <li><a href="#">Sitemap</a></li>
+            <li><a href="#">Careers</a></li>
+            <li><a href="#">Accessibility Adjustments</a></li>
+            <li><a href="#">Privacy Policy</a></li>
+        </ul>
+        <p>&copy; Organic Grocery Store 2024. All rights reserved.</p>
+    </footer>
+
+</body>
+
+</html>
